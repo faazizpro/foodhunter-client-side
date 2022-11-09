818a7b079c2de8531e29c46ba8d90aa72652645c
faazizpro <fahadafs11@gmail.com>
2022-11-10 01:34:42 +0600

    2 Hero Section has been done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
 import HeroInfo from '../HeroInfo/HeroInfo';
-import NavBar from '../NavBar.js/NavBar';
+import HomeCard from '../HomeCard/HomeCard';
 
 const Home = () => {
     return (
         <div>
             <Hero></Hero>
+            <HomeCard></HomeCard>
             <HeroInfo></HeroInfo>
         </div>
     );

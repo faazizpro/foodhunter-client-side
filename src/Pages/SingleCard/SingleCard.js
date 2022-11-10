b1909b1c8dcbd98139e931cee24338c6a75ebefd
faazizpro <fahadafs11@gmail.com>
2022-11-10 12:24:03 +0600

    Service Page> Service Section Done

--- src/Pages/SingleCard/SingleCard.js
+++ src/Pages/SingleCard/SingleCard.js
@@ -1,34 +1,55 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const SingleCard = () => {
     const { name, category, instruction, info, time, price, img } = useLoaderData();
 
     return (
         <div className='lg:flex mb-10 justify-center gap-5'>
-            {/* <div className='columns-10 border-2 border-red-500 grid grid-cols-2 mx-auto'> */}
-                <div class="max-w-2xl overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
-                    <img class="object-cover w-full h-64" src="https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Article"/>
+            <div class="max-w-2xl overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 lg:w-full w-4/5 mx-auto md:mx-0">
+                <PhotoProvider>
+                    <PhotoView src={img}>
+                        <img
+                            src={img}
+                            className="object-cover w-full h-80" />
+                    </PhotoView>
+                </PhotoProvider>
 
-                        <div class="p-6">
-                            <div>
-                                <span class="text-xs font-medium text-blue-600 uppercase dark:text-blue-400">Product</span>
-                                <a href="#" class="block mt-2 text-2xl font-semibold text-gray-800 transition-colors duration-300 transform dark:text-white hover:text-gray-600 hover:underline" tabindex="0" role="link">I Built A Successful Blog In One Year</a>
-                                <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie parturient et sem ipsum volutpat vel. Natoque sem et aliquam mauris egestas quam volutpat viverra. In pretium nec senectus erat. Et malesuada lobortis.</p>
-                            </div>
+                <div class="p-6">
+                    <div>
+                        <h3 class="text-xs md:text-2xl font-medium text-blue-600 uppercase dark:text-blue-400">{name}</h3>
 
-                            <div class="mt-4">
-                                <div class="flex items-center">
-                                    <div class="flex items-center">
-                                        <img class="object-cover h-10 rounded-full" src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=48&q=60" alt="Avatar" />
-                                        <a href="#" class="mx-2 font-semibold text-gray-700 dark:text-gray-200" tabindex="0" role="link">Jone Doe</a>
-                                    </div>
-                                    <span class="mx-1 text-xs text-gray-600 dark:text-gray-300">21 SEP 2015</span>
-                                </div>
+                        <p class="mt-2 text-sm md:text-base text-white">{info}</p>
+
+                        <h3 class="text-xs md:text-2xl font-medium text-blue-600 uppercase dark:text-blue-400 mt-5">Ingredients</h3>
+
+                        <p class="mt-2 text-sm md:text-base text-white">
+                            {instruction}
+                        </p>
+
+                        <div class="p-1.5 w-full inline-block sm:w-auto overflow-hidden bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-700 mt-5">
+                            <div class="space-y-2 sm:space-y-0 sm:flex sm:-mx-1">
+                                <button class="w-full px-4 py-2 text-white md:text-xl md:font-medium transition-colors duration-300 transform bg-blue-600 rounded-md focus:outline-none sm:w-auto sm:mx-1 ">
+                                    Category: {category}
+                                </button>
+
+                                <button class="w-full px-4 py-1 text-white md:text-xl md:font-medium transition-colors duration-300 transform bg-blue-600 rounded-md focus:outline-none sm:w-auto sm:mx-1 ">
+                                    Cooking Time: {time}
+                                </button>
+
+                                <button class="w-full px-4 py-1 text-white md:text-xl md:font-medium transition-colors duration-300 transform bg-blue-600 rounded-md focus:outline-none sm:w-auto sm:mx-1 ">
+                                    Price: ${price}
+                                </button>
                             </div>
                         </div>
+                    </div>
+
+
                 </div>
-            {/* </div> */}
+            </div>
+
             <div className='columns-3 border-2 border-green-400'>
                 <h1>This is review layout</h1>
             </div>
